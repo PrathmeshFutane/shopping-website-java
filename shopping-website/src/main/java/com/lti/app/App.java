307ac72fddf0433050cc1b0526844244dd10b11d
shreyash304 <2013shreyash@gmail.com>
2021-04-02 12:51:23 +0530

    new change wo bhi final

--- shopping-website/src/main/java/com/lti/app/App.java
+++ shopping-website/src/main/java/com/lti/app/App.java
@@ -3,7 +3,7 @@ package com.lti.app;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		System.out.println("new change done by shreyash");
 	
 	
