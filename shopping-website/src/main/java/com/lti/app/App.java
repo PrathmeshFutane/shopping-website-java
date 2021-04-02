6f574bac67c23f77b3d5b3ccac51187455536f66
PrathmeshFutane <63443691+PrathmeshFutane@users.noreply.github.com>
2021-04-02 12:45:00 +0530

    after adding build path

--- shopping-website/src/main/java/com/lti/app/App.java
+++ shopping-website/src/main/java/com/lti/app/App.java
@@ -4,10 +4,9 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this change is done by shreyash");
-		System.out.println("this change is done by Jeshma");
-		System.out.println("this is by prathmesh");
-		System.out.println("change by muskaan");
+		System.out.println("this is final project");
 	}
+	
+	
 
 }
